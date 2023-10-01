08cca9a29dfce9a40b0875f4bdbd50d686631a42
tih4r3-chan <ticampuoli18@gmail.com>
2023-10-01 01:26:36 -0300

    new
    listar los usuarios de la base de datos

--- src/app/pages/admin-log/admin/admin.page.ts
+++ src/app/pages/admin-log/admin/admin.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/firestore.service';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin',
@@ -7,12 +10,33 @@ import { AuthenticationService } from 'src/app/services/firestore.service';
   styleUrls: ['./admin.page.scss'],
 })
 export class AdminPage implements OnInit {
+  //inicializar la lisa
+  usuarios$: Observable<any[]>;
+  private usuariosCollection: AngularFirestoreCollection<any>; // Definimos la colección
 
   constructor(
-    public authService : AuthenticationService
-  ) { }
+    public authService : AuthenticationService,
+    private afStore: AngularFirestore
+  ) {
+    //inicializar coleccion
+    this.usuariosCollection = this.afStore.collection('usuarios');
+  }
 
   ngOnInit() {
+    this.listar();
+  }
+
+  listar() {
+    //hacer consulta a los documentos de la coleccion usuarios, hy estara actualizada
+    this.usuarios$ = this.usuariosCollection.snapshotChanges().pipe(
+      map((actions) => {
+        return actions.map((a) =>{
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return {id, ...data};
+        })
+      })
+    )
   }
 
 }
