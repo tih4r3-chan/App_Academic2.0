04a91fe3601a4f90a7d2b49d0b62e84d7add9e38
tih4r3-chan <ticampuoli18@gmail.com>
2023-10-01 01:41:59 -0300

    new
    agregue comentarios para recordar todo

--- src/app/pages/admin-log/admin/admin.page.ts
+++ src/app/pages/admin-log/admin/admin.page.ts
@@ -28,15 +28,22 @@ export class AdminPage implements OnInit {
 
   listar() {
     //hacer consulta a los documentos de la coleccion usuarios, hy estara actualizada
+    //snapshotChanges  metodo de AngularFirestoreCollection, devuelev observables, y se va actualizando junto a la base d edatos
+    //pipe --> operador  que se utiliza para encadenar operaciones en un flujo de datos observable
     this.usuarios$ = this.usuariosCollection.snapshotChanges().pipe(
+      // operacion que se aplica a los cambios del snap, actions obtiene los cambios sobre la coleccion
       map((actions) => {
+        //iteracion de cambios indviduales sobre la coleciion(1 por 1)
         return actions.map((a) =>{
+          //extrae datos del documento asociados al cabios, actualizado, todo lo relacionado con los usuarios
           const data = a.payload.doc.data();
+          //extrael el UID del documento, unico
           const id = a.payload.doc.id;
+          //se crea un objeto que combina el id con los datos de users
           return {id, ...data};
-        })
+        });
       })
-    )
+    );
   }
 
 }
