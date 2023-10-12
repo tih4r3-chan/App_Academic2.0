998657d84baab5c56a68ea02f6244935f3100cab
tih4r3-chan <ticampuoli18@gmail.com>
2023-10-12 16:00:48 -0300

    new
    provando asistencia

--- src/app/pages/docente/generador-asis/generador-asis.page.ts
+++ src/app/pages/docente/generador-asis/generador-asis.page.ts
@@ -1,5 +1,6 @@
 import { AsistenciaService } from 'src/app/services/asistencia.service';
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-generador-asis',
@@ -8,16 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneradorAsisPage implements OnInit {
 
+  //inicializando
+  firestoreData: any[];
+
   constructor(
     private asistenciaService: AsistenciaService,
+    private firestore: AngularFirestore
   ) { }
 
   ngOnInit() {
+    //mostrar datos de la clase uwu, Angular fire /
+    this.firestore.collection('clase').valueChanges()
+    .subscribe((data) => {
+      this.firestoreData = data;
+    })
   }
 
   //asistencia clase 1 --> 6bPLYxFBlJ6EcnYObJi6
-  newDoc1(){
-    this.asistenciaService.crearDoc1();
+  newDoc(){
+    this.asistenciaService.crearDoc();
   }
 
 }
