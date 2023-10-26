069d86067a337bd57984033fa093b9e3de474a28
tih4r3-chan <ticampuoli18@gmail.com>
2023-10-26 13:25:48 -0300

    new
    actualizando

--- src/app/pages/alumno/cuenta/cuenta.page.ts
+++ src/app/pages/alumno/cuenta/cuenta.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-cuenta',
@@ -6,17 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cuenta.page.scss'],
 })
 export class CuentaPage implements OnInit {
-  //usuarios es un lista de datos de usuarios en duro para mostrar
-  nombre: string = 'Tihare Campusano';
-  usuario: string = 'ti.campusano@duocuc.cl';
-  tipo: string = 'alumno';
-  telefono: string = '+569 35562325';
-  direccion: string = 'Viña del mar';
 
+  userData: any;
+  users: any;
+  userList: any[];
 
-  constructor() { }
+  constructor(
+    private service: ApiService
+  ) { }
 
   ngOnInit() {
+    this.leerUSer();
+
+    //obtener lista de user de la pai
+    this.service.getUsers().subscribe((data) => {
+      this.userList =data;
+
+      //compara el uid estraido con el amacenado
+      this.userList.forEach((user)=>{
+        //este es uid almacenado en capacitor
+        const uid = user.uid;
+        if(this.userData && this.userData.uid === uid){
+          this.userData = user;
+        }
+      })
+    })
+  }
+
+  //trae los datos del capcitor que estsan almacenados
+  async leerUSer(){
+    const response  = await Preferences.get({key:'user'});
+    if(response.value){
+      this.userData = JSON.parse(response.value);
+      //me trae el id del usuario
+      const idUser = this.userData.uid;
+      // console.log(idUser);
+    }
   }
 
 }
