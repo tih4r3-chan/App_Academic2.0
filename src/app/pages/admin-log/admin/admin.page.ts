d2ae7cc3d19f46a2412a6dcc99b6d9930751db3b
tih4r3-chan <ticampuoli18@gmail.com>
2023-10-27 03:43:14 -0300

    new
    aaaaa

--- src/app/pages/admin-log/admin/admin.page.ts
+++ src/app/pages/admin-log/admin/admin.page.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/firestore.service';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { ApiService } from 'src/app/services/api.service';
-import { claseModel } from 'src/app/models/clase';
 
 
 @Component({
@@ -13,21 +11,13 @@ import { claseModel } from 'src/app/models/clase';
   styleUrls: ['./admin.page.scss'],
 })
 export class AdminPage implements OnInit {
-  //inicializar la lisa
-  users: any[]=[];
-  usuarios$: Observable<any[]>;
-  private usuariosCollection: AngularFirestoreCollection<any>; // Definimos la colección
-
   //inicializando
   userList: any[];
 
   constructor(
     public authService : AuthenticationService,
-    private afStore: AngularFirestore,
     private _userServce: ApiService
   ) {
-    //inicializar coleccion
-    this.usuariosCollection = this.afStore.collection('usuarios');
   }
 
   ngOnInit() {
