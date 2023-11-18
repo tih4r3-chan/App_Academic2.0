1acd3b846bb6b7b712f8eb71bcde9e2b3d9c9e3f
tih4r3-chan <ticampuoli18@gmail.com>
2023-11-18 16:48:43 -0300

    new
    s

--- src/app/services/shared-data.service.ts
+++ src/app/services/shared-data.service.ts
@@ -5,10 +5,14 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class SharedDataService {
+  //se creo un BehaviorSubject llamado resultSource que inicializa con una cadena vacía
   private resultSource = new BehaviorSubject<string>('');
+  // sed ecalra currentResult como observable que emite lo valores del anterior
   currentResult = this.resultSource.asObservable();
 
-  setResult(result: string) {
+  //actualiza datos
+  setResult(result: any) {
+    // se pasa el resultado
     this.resultSource.next(result);
   }
 }
