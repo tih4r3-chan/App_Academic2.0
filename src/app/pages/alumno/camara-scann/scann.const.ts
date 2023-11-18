689aebecdcc372b8300b35f64696c8e76f7a3e2e
tih4r3-chan <ticampuoli18@gmail.com>
2023-11-18 15:03:26 -0300

    a
    a

--- src/app/pages/alumno/camara-scann/scann.const.ts
+++ src/app/pages/alumno/camara-scann/scann.const.ts
@@ -2,6 +2,7 @@ export const VIDEO_CONFIG ={
   video:{
     width:{min: 250,max:350,ideal:300},
     height:{min: 250,max:350,ideal:300},
-    facingMode: 'enviroment'
+    facingMode: 'enviroment',
+    video: 'someValue'
   }
 }
