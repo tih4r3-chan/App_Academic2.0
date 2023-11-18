872f5a3e8f68256860afbecdd6e9dcbd175423d5
tih4r3-chan <ticampuoli18@gmail.com>
2023-11-18 14:28:33 -0300

    new
    arreglando la cmara

--- src/app/pages/alumno/camara-scann/camara-scann.component.ts
+++ src/app/pages/alumno/camara-scann/camara-scann.component.ts
@@ -2,6 +2,8 @@ import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angul
 import jsQR from "jsqr";
 import {Subject, takeUntil, timer} from "rxjs";
 import { VIDEO_CONFIG } from './scann.const';
+import { Platform } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-camara-scann',
@@ -9,7 +11,7 @@ import { VIDEO_CONFIG } from './scann.const';
   styleUrls: ['./camara-scann.component.scss'],
 })
 
-export class CamaraScannComponent implements AfterViewInit{
+export class CamaraScannComponent implements OnDestroy{
   @ViewChild('videoElement') video!: ElementRef<HTMLVideoElement>
   @ViewChild('canvas', {static: true}) canvas!: ElementRef
 
@@ -20,80 +22,88 @@ export class CamaraScannComponent implements AfterViewInit{
 
   result = ''
 
-  ngAfterViewInit(): void {
-    this.prepareScanner()
-  }
+  constructor(private platform: Platform) {}
 
+  ngOnInit(): void {
+    this.platform.ready().then(() => {
+      this.prepareScanner();
+    });
+  }
   async prepareScanner() {
     const available = await this.checkCamera()
     if (available) this.startScanner()
   }
 
   changeCamera() {
-    let {facingMode} = this.config.video
+    let { facingMode } = this.config.video;
 
-    this.config.video.facingMode = facingMode === 'enviroment' ? 'user' : 'enviroment'
-    this.startScanner()
+    this.config.video.facingMode = facingMode === 'environment' ? 'user' : 'environment';
+    this.startScanner();
   }
 
   async startScanner() {
-    this.videoStream = await navigator.mediaDevices.getUserMedia(this.config)
-    this.video.nativeElement.srcObject = this.videoStream
+    try {
+      this.videoStream = await navigator.mediaDevices.getUserMedia(this.config);
+      this.video.nativeElement.srcObject = this.videoStream;
 
-    this.spyCamera()
+      this.video.nativeElement.addEventListener('canplay', () => {
+        this.spyCamera();
+      });
+    } catch (error) {
+      console.error('Error al obtener el stream de video: ', error);
+    }
   }
 
   spyCamera() {
     if (this.video.nativeElement) {
-      const {clientWidth, clientHeight} = this.video.nativeElement
+      const { clientWidth, clientHeight } = this.video.nativeElement;
 
-      this.canvas.nativeElement.width = clientWidth
-      this.canvas.nativeElement.height = clientHeight
+      this.canvas.nativeElement.width = clientWidth;
+      this.canvas.nativeElement.height = clientHeight;
 
-      const canvas = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D
+      const canvas = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
 
-      canvas.drawImage(this.video.nativeElement, 0, 0, clientWidth, clientHeight)
+      canvas.drawImage(this.video.nativeElement, 0, 0, clientWidth, clientHeight);
 
-      const inversionAttempts = 'dontInvert'
+      const inversionAttempts = 'dontInvert';
 
-      const image = canvas.getImageData(0, 0, clientWidth, clientHeight)
-      const qrcode = jsQR(image.data, image.width, clientHeight, {inversionAttempts})
+      const image = canvas.getImageData(0, 0, clientWidth, clientHeight);
+      const qrcode = jsQR(image.data, image.width, clientHeight, { inversionAttempts });
 
       if (qrcode) {
-        const {data} = qrcode
-        this.result = ""
-        this.result = data
-
+        const { data } = qrcode;
+        this.result = data;
       } else {
-        timer(100).pipe(takeUntil(this.destroy$)).subscribe(() => {
-          this.spyCamera()
-        })
+        timer(100)
+          .pipe(takeUntil(this.destroy$))
+          .subscribe(() => {
+            this.spyCamera();
+          });
       }
     }
   }
 
-  async checkCamera() {
-    const cameraPermissions = await navigator.permissions.query({name: 'camera'} as any)
+  async checkCamera(): Promise<boolean> {
+    const cameraPermissions = await navigator.permissions.query({ name: 'camera' } as any);
 
-    const isOk = cameraPermissions.state !== "denied"
+    const isOk = cameraPermissions.state !== 'denied';
 
-    const hasMediaDevice = 'mediaDevices' in navigator
-    const hasUserMedia = 'getUserMedia' in navigator.mediaDevices
+    const hasMediaDevice = 'mediaDevices' in navigator;
+    const hasUserMedia = 'getUserMedia' in navigator.mediaDevices;
 
     if (!hasMediaDevice || (!hasUserMedia && isOk)) {
-      alert("Nao conseguimos acesso a camera, por favor verifique")
+      alert('No se pudo acceder a la cámara. Por favor, verifique la configuración de permisos.');
     }
 
-    return cameraPermissions.state !== "denied"
+    return cameraPermissions.state !== 'denied';
   }
-
 
   ngOnDestroy() {
-    this.videoStream.getTracks().forEach((track) => track.stop())
-    this.video = null!
+    if (this.videoStream) {
+      this.videoStream.getTracks().forEach((track) => track.stop());
+    }
 
-    this.destroy$.next()
-    this.destroy$.complete()
+    this.destroy$.next();
+    this.destroy$.complete();
   }
-
 }
